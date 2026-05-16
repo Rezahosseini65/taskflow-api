f0d39843c28433e7f0432c42a015b21d1fbebc8b
Reza <hosseini.reza65@gmail.com>
2026-05-16 14:50:38 +0330

    feat: add debug toolbar and static/media serving in DEBUG mode
    - Add debug_toolbar URLs under __debug__/ path
    - Conditionally serve static and media files when DEBUG is True
    - Import settings and static helper functions

--- src/taskflow/urls.py
+++ src/taskflow/urls.py
@@ -1,13 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('__debug__/', include("debug_toolbar.urls")),
     path('api/users/', include('taskflow.accounts.urls'), name='register-user'),
     path('api/boards/', include('taskflow.boards.urls'), name='create-board'),
 
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
