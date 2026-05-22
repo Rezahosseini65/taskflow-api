5f070160c18daa508c80d5b225066b0a40a83c39
Reza <hosseini.reza65@gmail.com>
2026-05-22 18:45:51 +0330

    refactor(boards): remove ListAdmin for deleted List model

--- src/taskflow/boards/admin.py
+++ src/taskflow/boards/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Board, List
+from .models import Board
 
 
 @admin.register(Board)
@@ -22,28 +22,6 @@ class BoardAdmin(admin.ModelAdmin):
         }),
         ('Ownership & Membership', {
             'fields': ('owner', 'members'),
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        }),
-    )
-
-
-@admin.register(List)
-class ListAdmin(admin.ModelAdmin):
-
-    list_display = ('name', 'board', 'position', 'created_at', 'updated_at')
-
-    list_filter = ('board', 'created_at')
-
-    search_fields = ('name', 'board__name')
-
-    readonly_fields = ('created_at', 'updated_at')
-
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'board', 'position')
         }),
         ('Timestamps', {
             'fields': ('created_at', 'updated_at'),
